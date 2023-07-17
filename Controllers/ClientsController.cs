ab7ecffb1075387ccca8193dfebf1e245907e255
OMAR E. ROMERO <omar_erg@hotmail.com>
2023-07-17 16:02:37 -0600

    [ADD] Initializing project

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionClientes.Controllers
+{
+    public class ClientsController : Controller
+    {
+        // GET: ClientsController
+        public ActionResult Clients()
+        {
+            return View();
+        }
+    }
+}
